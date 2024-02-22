d55fef18a37e2d3baca29983ef5f31e53e8ed8fd
andrejsmattos <andrejusmattos@gmail.com>
2024-02-22 14:53:22 -0300

    [M1S07] Ex. 1
    Input.value é adicionado à lista, se não estiver em branco, e retorna a ficar em branco depois de adicionado à lista

--- To-do List/script.js
+++ To-do List/script.js
@@ -4,13 +4,18 @@ function addTarefa() {
   let inputTarefa = document.querySelector('#input-tarefa').value;
   let list = document.querySelector('.list');
 
+  if (inputTarefa === "") {
+    alert("Escreva alguma tarefa para adicioná-la a lista"); // [M1S07] Ex. 1 - SE A TAREFA É ADICIONADA À LISTA SEM SER PREENCHIDA É GERADO UM ALERT
+  } else {
   let li = document.createElement("li");
   li.className = "li-items";
+  // [M1S07] Ex. 1 - NOVO ITEM É ADICIONADO À LISTA AO CLICAR NO BOTÃO "ADICIONAR"
   li.innerHTML = `<input type='checkbox' class='input-li'> <span>${inputTarefa}</span> <button class='erase' onclick="removeTarefa(this)"><i class="fa-regular fa-circle-xmark icon"></i></button>`;
   
   list.appendChild(li);
   
-  document.querySelector('#input-tarefa').value = "";
+  document.querySelector('#input-tarefa').value = ""; // [M1S07] Ex. 1 - TEXTO DE INPUT É APAGADO DEPOIS DE ADICIONADO À LISTA
+  }
 }
 
 // REMOVER <LI> DA LISTA AO CLICAR NO BOTÃO 'X'
@@ -19,3 +24,5 @@ function removeTarefa(button) {
   
   liToRemove.remove();
 }
+
+
